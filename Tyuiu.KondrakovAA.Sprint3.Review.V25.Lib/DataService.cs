938a47f824bb16925388786cb6eb3e512c0bb82c
KondrakovAlexander <144276548+KondrakovAlexander@users.noreply.github.com>
2023-12-27 17:32:14 +0500

    Add files via upload

---
+++ Tyuiu.KondrakovAA.Sprint3.Review.V25.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KondrakovAA.Sprint3.Review.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int start, int stop)
+        {
+            double[] valueArray;
+            int len = (stop - start) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = start; x <= stop; x++)
+            {
+                y = Math.Round(Math.Cos(x) + (4 * x)/ 2 - Math.Sin(x) * (3 * x), 2);
+                valueArray[count] = y;
+                count++; 
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.KondrakovAA.Sprint3.Review.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KondrakovAA.Sprint3.Review.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KondrakovAA.Sprint3.Review.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4af50cab-42c2-49ca-aff0-6487f0e25262")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
